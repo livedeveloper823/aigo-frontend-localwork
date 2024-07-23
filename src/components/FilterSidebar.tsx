0f1a883085231dfdd4d5c80653e9656b00abc6cc
softdev0810 <laideming935@gmail>
2024-07-23 22:44:46 +1000

    keep filter items

--- src/components/FilterSidebar.tsx
+++ src/components/FilterSidebar.tsx
@@ -2,6 +2,7 @@ import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { Checkbox } from "@/components/ui/checkbox";
 import { useEffect } from "react";
 import { CheckItem, RadioItem, FilterItem } from "@/types";
+import { FilterItems } from "@/content/FilterItems";
 
 interface FilterSidebarProps {
   checkedItems: CheckItem[];
@@ -16,207 +17,6 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
   radioItem,
   setRadioItem,
 }) => {
-  const FilterItem: FilterItem = {
-    CheckFilter: [
-      {
-        content: "> = + 100%",
-        limit: {
-          min: 0,
-          max: 100,
-        },
-      },
-      {
-        content: "+30% to +100%",
-        limit: {
-          min: 30,
-          max: 100,
-        },
-      },
-      {
-        content: "+5% to +30%",
-        limit: {
-          min: 5,
-          max: 30,
-        },
-      },
-      {
-        content: "-5% to -15%",
-        limit: {
-          min: -15,
-          max: -5,
-        },
-      },
-      {
-        content: "-30% to -100%",
-        limit: {
-          min: -100,
-          max: -30,
-        },
-      },
-    ],
-    RadioFilter: {
-      MarketCap: [
-        { title: "Market Cap" },
-        {
-          content: "> = $1b",
-          value: "0 1000000000",
-        },
-        {
-          content: "$100m to $1b",
-          value: "100000000 1000000000",
-        },
-        {
-          content: "$10m to $100m",
-          value: "10000000 100000000",
-        },
-        {
-          content: "< = $10m",
-          value: "10000000 0",
-        },
-      ],
-      InTheMoney: [
-        { title: "In The Money" },
-        {
-          content: "> = 95%",
-          value: "0 95",
-        },
-        {
-          content: "75% to 100%",
-          value: "75 100",
-        },
-        {
-          content: "25% to 75%",
-          value: "25 75",
-        },
-        {
-          content: "< = 25%",
-          value: "25 0",
-        },
-      ],
-      LargeTxs: [
-        { title: "Large Txs Volume" },
-        {
-          content: "> = $1b",
-          value: "0 1000000000",
-        },
-        {
-          content: "$100m to $1b",
-          value: "100000000 1000000000",
-        },
-        {
-          content: "$10m to $100m",
-          value: "10000000 100000000",
-        },
-        {
-          content: "< = $10m",
-          value: "10000000 0",
-        },
-      ],
-      DailyActive: [
-        { title: "Daily Active Addresses" },
-        {
-          content: "> = 100k",
-          value: "0 100000",
-        },
-        {
-          content: "$10k to $100k",
-          value: "10000 100000",
-        },
-        {
-          content: "$1k to $10k",
-          value: "1000 10000",
-        },
-        {
-          content: "< = $1k",
-          value: "1000 0",
-        },
-      ],
-      Ownership: [
-        { title: "Ownership Distribution" },
-        {
-          content: "Whales > Investors",
-          value: "Whales > Investors",
-        },
-        {
-          content: "Investors > Whales",
-          value: "Investors > Whales",
-        },
-        {
-          content: "Retailers > Investors",
-          value: "Retailers > Investors",
-        },
-      ],
-      Hodlers: [
-        { title: "Hodlers Balance" },
-        {
-          content: "> = $1b",
-          value: "0 1000000000",
-        },
-        {
-          content: "$100m to $1b",
-          value: "100000000 1000000000",
-        },
-        {
-          content: "$10m to $100m",
-          value: "10000000 100000000",
-        },
-        {
-          content: "< = $10m",
-          value: "10000000 0",
-        },
-      ],
-      Twitter: [
-        { title: "Twitter Sentiment" },
-        {
-          content: "Position",
-          value: "Position",
-        },
-        {
-          content: "Neutral",
-          value: "Neutral",
-        },
-        {
-          content: "Negative",
-          value: "Negative",
-        },
-      ],
-      Exchanges: [
-        { title: "Exchanges Netflows" },
-        {
-          content: "> = $10m",
-          value: "0 10000",
-        },
-        {
-          content: "$1m to $10m",
-          value: "1000 10000",
-        },
-        {
-          content: "-$1m to -$10m",
-          value: "-10000 -1000",
-        },
-        {
-          content: "< = $10m",
-          value: "10000 0",
-        },
-      ],
-      Signals: [
-        { title: "Signals" },
-        {
-          content: "Bullish",
-          value: "Bullish",
-        },
-        {
-          content: "Neutral",
-          value: "Neutral",
-        },
-        {
-          content: "Bearish",
-          value: "Bearish",
-        },
-      ],
-    },
-  };
-
   const handleCheck = (item: CheckItem) => {
     if (checkedItems.some((i) => i.content === item.content)) {
       console.log("remove: ", item.content);
@@ -246,7 +46,7 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
     <div className="flex flex-col gap-6 p-5 w-72 text-white">
       <div className="flex flex-col gap-2">
         <p className="font-semibold">Price Change</p>
-        {FilterItem.CheckFilter.map((item, index) => (
+        {FilterItems.CheckFilter.map((item, index) => (
           <div className="flex gap-2 items-center" key={index}>
             <Checkbox
               id={`check${index}`}
@@ -259,7 +59,7 @@ const FilterSidebar: React.FC<FilterSidebarProps> = ({
         ))}
       </div>
 
-      {Object.entries(FilterItem.RadioFilter).map(([key, items]) => (
+      {Object.entries(FilterItems.RadioFilter).map(([key, items]) => (
         <div key={key}>
           <p className="font-semibold">{items[0].title}</p>
           <RadioGroup>
